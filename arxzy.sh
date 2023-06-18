e79a1275e015c8e03fdb824f0bfd5e2249979b34
Dryan-MD <121863865+AdrianTzy@users.noreply.github.com>
2023-06-18 23:39:29 +0700

    INSTALL ALL SCRIPT

---
+++ arxzy.sh
@@ -0,0 +1,1 @@
+apt update && apt upgrade && apt install ffmpeg imagemagick nodejs curl unzip && curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - && sudo apt install nodejs && apt update && apt upgrade
